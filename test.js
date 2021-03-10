df5eed6bb659bb17c2fb7e78bf0604da0760ab9f
Gimenz <agaaja187@gmail.com>
2021-03-10 09:19:34 +0700

    Initial Commit

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ ig.fetchPost('https://www.instagram.com/p/CMBZUsMALA_/').then(res => {
  console.log(res)
 })
 
-/*ig.fetchUser('ochaameviaa').then(res => {
+/*ig.fetchUser('gimenz.id').then(res => {
   console.log(res)
 })*/
