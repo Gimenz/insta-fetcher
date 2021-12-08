9eaa8d978dd8150fd0bc14d772504973a72f2303
Gimenz <agaaja187@gmail.com>
2021-12-08 22:24:37 +0700

    ok

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 const ig = require('./');
 
-// ig.fetchPost('https://www.instagram.com/p/CMBZUsMALA_/').then(res => {
-//  console.log(res);
+// ig.fetchPost('https://www.instagram.com/p/CXOZbFrPfO-/?utm_source=ig_web_copy_link').then(res => {
+//   console.log(res);
 // })
 
 // recommended to set cookie first for call most all instagram endpoint
 //setCookie("45383374294%3AXmvVAsMceyYZ2S7%3A16")
 
-// ig.fetchUser('adiraas.p').then(res => {
-//   console.log(res);
-// })
-
-ig.explore('natgeo').then(res => {
+ig.fetchUser('adiraas.p').then(res => {
   console.log(res);
 })
+
+// ig.explore('natgeo').then(res => {
+//   console.log(res);
+// })
