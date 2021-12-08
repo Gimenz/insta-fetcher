de4c2ce6c4ee5bbbb252e75d6e2de02dac017726
Gimenz <agaaja187@gmail.com>
2021-12-08 21:15:17 +0700

    update + refactor code

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-const ig = require('./index');
+const ig = require('./');
 
-/*ig.fetchPost('https://www.instagram.com/p/CMBZUsMALA_/').then(res => {
- console.log(res);
-})
-*/
+// ig.fetchPost('https://www.instagram.com/p/CMBZUsMALA_/').then(res => {
+//  console.log(res);
+// })
 
-ig.fetchUser('adiraas.p').then(res => {
+// recommended to set cookie first for call most all instagram endpoint
+//setCookie("45383374294%3AXmvVAsMceyYZ2S7%3A16")
+
+// ig.fetchUser('adiraas.p').then(res => {
+//   console.log(res);
+// })
+
+ig.explore('natgeo').then(res => {
   console.log(res);
 })
